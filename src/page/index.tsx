9287708c81cf619786d33001b025d748fa1091b9
Gustavo Edson nunes <ggugu9289@gmail.com>
2025-11-19 21:53:24 -0300

    fix: api-call

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,3 +1,32 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 export const Home: React.FC = () => {
-  return <div></div>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://restcountries.com/v3.1/all?fields=name`
+      );
+      const res = await response.json();
+      setData(res);
+      console.log(data);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      {data ? (
+        <p>
+          {data.map((country, i) => (
+            <div key={i}>{country.name}</div>
+          ))}
+        </p>
+      ) : (
+        <p>loading...</p>
+      )}
+    </div>
+  );
 };
